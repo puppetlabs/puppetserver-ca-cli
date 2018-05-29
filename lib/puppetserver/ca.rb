87cbc6f3fa420559d3b00f08d1507db784c48804
Justin Stoller <justin.stoller@gmail.com>
2018-05-29 13:51:52 -0700

    (SERVER-2172) Create skeleton project
    This is the basic skeleton project created by `bundle gem
    puppetserver-ca`.

---
+++ lib/puppetserver/ca.rb
@@ -0,0 +1,7 @@
+require "puppetserver/ca/version"
+
+module Puppetserver
+  module Ca
+    # Your code goes here...
+  end
+end
---
+++ lib/puppetserver/ca/version.rb
@@ -0,0 +1,5 @@
+module Puppetserver
+  module Ca
+    VERSION = "0.1.0"
+  end
+end
