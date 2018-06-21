91a28297e71ded3d163abcbf3743537822fe2968
Justin Stoller <justin.stoller@gmail.com>
2018-06-20 22:03:21 -0700

    (SERVER-2172) Refactor specs for readability

--- spec/puppetserver/ca/cli_spec.rb
+++ spec/puppetserver/ca/cli_spec.rb
@@ -9,48 +9,45 @@ RSpec.describe Puppetserver::Ca::Cli do
   let(:stdout) { StringIO.new }
   let(:stderr) { StringIO.new }
 
-  it 'responds to a --help flag' do
-    exit_code = Puppetserver::Ca::Cli.run!(['--help'], stdout, stderr)
-    expect(stdout.string).to include('usage: puppetserver ca <command> [options]')
-    expect(stdout.string).to include('This general help output')
-    expect(exit_code).to be 0
-  end
-
-  it 'prints the help output & returns 1 if no input is given' do
-    exit_code = Puppetserver::Ca::Cli.run!([], stdout, stderr)
-    expect(stderr.string).to include('This general help output')
-    expect(exit_code).to be 1
-  end
-
-  it 'prints the version regardless of subcommand' do
-    out1, err1 = StringIO.new, StringIO.new
-    first_code = Puppetserver::Ca::Cli.run!(['--version'], out1, err1)
-    expect(out1.string).to match(/\d+\.\d+\.\d+(-[a-z0-9._-]+)?/)
-    expect(err1.string).to be_empty
-    expect(first_code).to be 0
-
-    out2, err2 = StringIO.new, StringIO.new
-    second_code = Puppetserver::Ca::Cli.run!(['setup', '--version'],
-                                             out2,
-                                             err2)
-    expect(out2.string).to match(/\d+\.\d+\.\d+(-[a-z0-9._-]+)?/)
-    expect(err2.string).to be_empty
-    expect(second_code).to be 0
-  end
-
-  describe 'the setup subcommand' do
-    it 'responds with a specific help output when given the --help flag"' do
-      exit_code = Puppetserver::Ca::Cli.run!(['setup', '--help'], stdout, stderr)
-      expect(stdout.string).to include('usage: puppetserver ca setup [options]')
-      expect(stdout.string).to include('This setup specific help output')
+  shared_examples 'basic cli args' do |subcommand, usage|
+    it 'responds to a --help flag' do
+      args = [subcommand, '--help'].compact
+      exit_code = Puppetserver::Ca::Cli.run!(args, stdout, stderr)
+      expect(stdout.string).to match(usage)
       expect(exit_code).to be 0
     end
 
     it 'prints the help output & returns 1 if no input is given' do
-      exit_code = Puppetserver::Ca::Cli.run!(['setup'], stdout, stderr)
-      expect(stderr.string).to include('This setup specific help output')
+      args = [subcommand].compact
+      exit_code = Puppetserver::Ca::Cli.run!(args, stdout, stderr)
+      expect(stderr.string).to match(usage)
       expect(exit_code).to be 1
     end
+
+    it 'prints the version' do
+      semverish = /\d+\.\d+\.\d+(-[a-z0-9._-]+)?/
+      args = [subcommand, '--version'].compact
+      first_code = Puppetserver::Ca::Cli.run!(args, stdout, stderr)
+      expect(stdout.string).to match(semverish)
+      expect(stderr.string).to be_empty
+      expect(first_code).to be 0
+    end
+  end
+
+  describe 'general options' do
+    include_examples 'basic cli args',
+      nil,
+      /.*usage: puppetserver ca <command> .*This general help output.*/m
+  end
+
+  describe 'the setup subcommand' do
+    let(:usage) do
+      /.*usage: puppetserver ca setup.*This setup specific help output.*/m
+    end
+
+    include_examples 'basic cli args',
+      'setup',
+      /.*usage: puppetserver ca setup.*This setup specific help output.*/m
 
     it 'does not print the help output if called correctly' do
       Dir.mktmpdir do |tmpdir|
@@ -68,52 +65,48 @@ RSpec.describe Puppetserver::Ca::Cli do
       end
     end
 
-    context 'required options' do
-      it 'requires the --cert-bundle if only given the --private-key' do
+    context 'validation' do
+      it 'requires both the --cert-bundle and --private-key options' do
         exit_code = Puppetserver::Ca::Cli.run!(
                       ['setup', '--private-key', 'foo'],
                       stdout,
                       stderr)
-        expect(stderr.string).to include('Warning')
         expect(stderr.string).to include('missing required argument')
-        expect(stderr.string).to include('usage: ')
+        expect(stderr.string).to match(usage)
         expect(exit_code).to be 1
-      end
 
-      it 'requires the --private-key if only given the --cert-bundle' do
         exit_code = Puppetserver::Ca::Cli.run!(
                       ['setup', '--cert-bundle', 'foo'],
                       stdout,
                       stderr)
-        expect(stderr.string).to include('Warning')
         expect(stderr.string).to include('missing required argument')
-        expect(stderr.string).to include('usage: ')
+        expect(stderr.string).to match(usage)
         expect(exit_code).to be 1
       end
-    end
 
-    it 'warns when no crl is given' do
-      exit_code = Puppetserver::Ca::Cli.run!(
-                    ['setup',
-                     '--cert-bundle', 'foo',
-                     '--private-key', 'bar'],
-                    stdout,
-                    stderr)
-      expect(stderr.string).to include('Warning')
-      expect(stderr.string).to include('Full CRL chain checking will not be possible')
-    end
-
-    it 'requires cert-bundle, private-key, and crl-chain to be readable' do
-      Dir.mktmpdir do |tmpdir|
+      it 'warns when no CRL is given' do
         exit_code = Puppetserver::Ca::Cli.run!(
                       ['setup',
-                       '--cert-bundle', File.join(tmpdir, 'cert_bundle.pem'),
-                       '--private-key', File.join(tmpdir, 'private_key.pem'),
-                       '--crl-chain', File.join(tmpdir, 'crl_chain.pem')],
-                      stdout, stderr)
-        expect(stderr.string).to match(/Could not read .*cert_bundle.pem/)
-        expect(stderr.string).to match(/Could not read .*private_key.pem/)
-        expect(stderr.string).to match(/Could not read .*crl_chain.pem/)
+                       '--cert-bundle', 'foo',
+                       '--private-key', 'bar'],
+                      stdout,
+                      stderr)
+        expect(stderr.string).to include('Full CRL chain checking will not be possible')
+      end
+
+      it 'requires cert-bundle, private-key, and crl-chain to be readable' do
+        # All errors are surfaced from validations
+        Dir.mktmpdir do |tmpdir|
+          exit_code = Puppetserver::Ca::Cli.run!(
+                        ['setup',
+                         '--cert-bundle', File.join(tmpdir, 'cert_bundle.pem'),
+                         '--private-key', File.join(tmpdir, 'private_key.pem'),
+                         '--crl-chain', File.join(tmpdir, 'crl_chain.pem')],
+                        stdout, stderr)
+          expect(stderr.string).to match(/Could not read .*cert_bundle.pem/)
+          expect(stderr.string).to match(/Could not read .*private_key.pem/)
+          expect(stderr.string).to match(/Could not read .*crl_chain.pem/)
+        end
       end
     end
   end
