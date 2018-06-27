5346e7e44ed420e10a8c02a8f5daa55995ccd1c3
Justin Stoller <justin.stoller@gmail.com>
2018-06-27 00:12:22 -0700

    (SERVER-2172) Move x509 obj handling to dedicated class

--- lib/puppetserver/ca/cli.rb
+++ lib/puppetserver/ca/cli.rb
@@ -99,7 +99,124 @@ module Puppetserver
 
         return parser, parsed
       end
+    end
 
+    class X509Loader
+
+      attr_reader :errors, :certs, :key, :crls
+      def initialize(bundle_file, key_file, chain_file)
+        @bundle_file = bundle_file
+        @key_file = key_file
+        @chain_file = chain_file
+
+        @certs, @key, @crls = nil, nil, nil
+
+        @errors = []
+      end
+
+      def load_and_validate!
+        @certs = parse_certs(@bundle_file)
+        @key = parse_key(@key_file)
+
+        @crls = @chain_file ? parse_crls(@chain_file) : []
+
+        unless @crls.empty? || @certs.empty?
+          validate_crl_and_cert(@crls.first, @certs.first)
+        end
+
+        if @key && !@certs.empty?
+          validate_cert_and_key(@key, @certs.first)
+        end
+
+        unless @certs.empty?
+          validate_full_chain(@certs, @crls)
+        end
+      end
+
+      def parse_certs(bundle)
+        errs = []
+        errs << "Could not parse #{bundle}"
+
+        bundle_string = File.read(bundle)
+        cert_strings = bundle_string.scan(/-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m)
+        certs = cert_strings.map do |cert_string|
+          begin
+            OpenSSL::X509::Certificate.new(cert_string)
+          rescue OpenSSL::X509::CertificateError
+            errs << "Could not parse entry:\n#{cert_string}"
+
+            nil
+          end
+        end.compact
+
+        if certs.empty?
+          errs << "Could not detect any certs within #{bundle}"
+        end
+
+        @errors += errs if errs.length > 1
+
+        return certs
+      end
+
+      def parse_key(key_path)
+        begin
+          OpenSSL::PKey.read(File.read(key_path))
+        rescue ArgumentError => e
+          @errors << "Could not parse #{key_path}"
+
+          return nil
+        end
+      end
+
+      def parse_crls(chain)
+        errs = []
+        errs << "Could not parse #{chain}"
+
+        chain_string = File.read(chain)
+        crl_strings = chain_string.scan(/-----BEGIN X509 CRL-----.*?-----END X509 CRL-----/m)
+        actual_crls = crl_strings.map do |crl_string|
+          begin
+            OpenSSL::X509::CRL.new(crl_string)
+          rescue OpenSSL::X509::CRLError
+            errs << "Could not parse entry:\n#{crl_string}"
+
+            nil
+          end
+        end.compact
+
+        if actual_crls.empty?
+          errs << "Could not detect any crls within #{chain}"
+        end
+
+        @errors += errs if errs.length > 1
+
+        return actual_crls
+      end
+
+      def validate_cert_and_key(key, cert)
+        unless cert.check_private_key(key)
+          @errors << 'Private key and certificate do not match'
+        end
+      end
+
+      def validate_crl_and_cert(crl, cert)
+        unless crl.issuer == cert.subject
+          @errors << 'Leaf CRL was not issued by leaf certificate'
+        end
+      end
+
+      def validate_full_chain(certs, crls)
+        store = OpenSSL::X509::Store.new
+        certs.each {|cert| store.add_cert(cert) }
+        if crls
+          store.flags = OpenSSL::X509::V_FLAG_CRL_CHECK | OpenSSL::X509::V_FLAG_CRL_CHECK_ALL
+          crls.each {|crl| store.add_crl(crl) }
+        end
+
+        unless store.verify(certs.first)
+          @errors << 'Leaf certificate could not be validated'
+        end
+      end
     end
 
     class Cli
@@ -114,41 +231,42 @@ module Puppetserver
 
             return exit_code if exit_code
 
-            begin
-              files = input.values_at('cert-bundle', 'private-key')
-              files << input['crl-chain'] if input['crl-chain']
-              files << input['config'] if input['config']
+            files = input.values_at('cert-bundle', 'private-key')
+            files << input['crl-chain'] if input['crl-chain']
+            files << input['config'] if input['config']
 
-              validate_file_paths(files)
-
-              certs = parse_certs(input['cert-bundle'])
-              key = parse_key(input['private-key'])
-              validate_cert_and_key(key, certs.first)
-
-              crls = nil
-              if input['crl-chain']
-                crls = parse_crls(input['crl-chain'])
-                validate_crl_and_cert(crls.first, certs.first)
-              else
-                err.puts 'Warning:'
-                err.puts '    No CRL chain given'
-                err.puts '    Full CRL chain checking will not be possible'
-                err.puts ''
-              end
-
-              validate_full_chain(certs, crls)
-
-              # do stuff
-              return 0
-
-            rescue CAError => e
+            errors = validate_file_paths(files)
+            unless errors.empty?
               err.puts "Error:"
-              err.puts "    #{e.to_s}" unless e.to_s.empty?
-              e.messages.each do |message|
+              errors.each do |message|
                 err.puts "    #{message}"
               end
               return 1
             end
+
+            unless input['crl-chain']
+              err.puts 'Warning:'
+              err.puts '    No CRL chain given'
+              err.puts '    Full CRL chain checking will not be possible'
+              err.puts ''
+            end
+
+            loader = X509Loader.new(input['cert-bundle'],
+                                    input['private-key'],
+                                    input['crl-chain'])
+
+            loader.load_and_validate!
+
+            unless loader.errors.empty?
+              err.puts "Error:"
+              loader.errors.each do |message|
+                err.puts "    #{message}"
+              end
+              return 1
+            end
+
+            # do stuff
+            return 0
           end
         else
           general_parser, input = parse_general_inputs(cli_args)
@@ -167,14 +285,14 @@ module Puppetserver
       end
 
       def self.validate_file_paths(one_or_more_paths)
-        error = CAError.new("")
+        errors = []
         Array(one_or_more_paths).each do |path|
           if !File.exist?(path) || !File.readable?(path)
-            error.add_message "Could not read file '#{path}'"
+            errors << "Could not read file '#{path}'"
           end
         end
 
-        raise error unless error.messages.empty?
+        errors
       end
 
       def self.parse_general_inputs(inputs)
@@ -194,82 +312,6 @@ module Puppetserver
         return general_parser, parsed
       end
 
-      def self.parse_certs(bundle)
-        error = CAError.new("Could not parse #{bundle}")
-
-        bundle_string = File.read(bundle)
-        cert_strings = bundle_string.scan(/-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m)
-        certs = cert_strings.map do |cert_string|
-          begin
-            OpenSSL::X509::Certificate.new(cert_string)
-          rescue OpenSSL::X509::CertificateError
-            error.add_message "Could not parse entry:\n#{cert_string}"
-          end
-        end
-
-        if certs.empty?
-          error.add_message "Could not detect any certs within #{bundle}"
-        end
-
-        raise error unless error.messages.empty?
-
-        return certs
-      end
-
-      def self.parse_key(key_path)
-        begin
-          OpenSSL::PKey.read(File.read(key_path))
-        rescue ArgumentError => e
-          raise CAError.new("Could not parse #{key_path}")
-        end
-      end
-
-      def self.validate_cert_and_key(key, cert)
-        unless cert.check_private_key(key)
-          raise CAError.new('Private key and certificate do not match')
-        end
-      end
-
-      def self.parse_crls(chain)
-        error = CAError.new("Could not parse #{chain}")
-
-        chain_string = File.read(chain)
-        crl_strings = chain_string.scan(/-----BEGIN X509 CRL-----.*?-----END X509 CRL-----/m)
-        crls = crl_strings.map do |crl_string|
-          begin
-            OpenSSL::X509::CRL.new(crl_string)
-          rescue OpenSSL::X509::CRLError
-            error.add_message "Could not parse entry:\n#{crl_string}"
-          end
-        end
-
-        if crls.empty?
-          error.add_message "Could not detect any crls within #{chain}"
-        end
-
-        raise error unless error.messages.empty?
-
-        return crls
-      end
-
-      def self.validate_crl_and_cert(crl, cert)
-        unless crl.issuer == cert.subject
-          raise CAError.new('Leaf CRL was not issued by leaf certificate')
-        end
-      end
-
-      def self.validate_full_chain(certs, crls)
-        store = OpenSSL::X509::Store.new
-        certs.each {|cert| store.add_cert(cert) }
-        if crls
-          store.flags = OpenSSL::X509::V_FLAG_CRL_CHECK | OpenSSL::X509::V_FLAG_CRL_CHECK_ALL
-          crls.each {|crl| store.add_crl(crl) }
-        end
-
-        unless store.verify(certs.first)
-          raise CAError.new('Leaf certificate could not be validated')
-        end
-      end
     end
   end
 end
