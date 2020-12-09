71e17a0e89705e7e0d3702d807886990746adbc8
Molly Waggett <molly.waggett@puppet.com>
2020-12-09 12:28:26 -0800

    (SERVER-2917) Ensure old cadir symlink has same perms as actual cadir
    This commit updates the `forcibly_symlink` method, used by the `setup`,
    `import`, and (now) `migrate` commands, to ensure that the ownership and
    permissions on the symlink to the old cadir match those on the new cadir.

--- lib/puppetserver/ca/utils/file_system.rb
+++ lib/puppetserver/ca/utils/file_system.rb
@@ -53,6 +53,14 @@ module Puppetserver
         def self.forcibly_symlink(source, link_target)
           FileUtils.remove_dir(link_target, true)
           FileUtils.symlink(source, link_target)
+          # Ensure the symlink has the same ownership as the source.
+          # This requires using `FileUtils.chown` rather than `File.chown`, as
+          # the latter will update the ownership of the source rather than the
+          # link itself.
+          # Symlink permissions are ignored in favor of the source's permissions,
+          # so we don't have to change those.
+          source_info = File.stat(source)
+          FileUtils.chown(source_info.uid, source_info.gid, link_target)
         end
 
         def initialize
